f692ac2b0d51c27655e180f14596197789553302
Uwe Oehmichen <u.oehmichen@queo-group.com>
2023-11-28 12:22:19 +0100

    AB#13 Formatcheck rausgenommen, da er über SonarCloud funktionieren soll.

--- build/Build/Flow/Flow.cs
+++ build/Build/Flow/Flow.cs
@@ -1,17 +1,16 @@
 using Cake.Frosting;
 
 namespace Build {
-    [IsDependentOn(typeof(FormatCheck))]
+    //[IsDependentOn(typeof(FormatCheck))]
     [IsDependentOn(typeof(NugetRestore))]
     [IsDependentOn(typeof(GenerateVersion))]
     [IsDependentOn(typeof(Build))]
     [IsDependentOn(typeof(RunTestsAndPublishResults))]
     [IsDependentOn(typeof(GetNuGetPackagesFromArtifacts))]
     [IsDependentOn(typeof(UploadArtifactsToPipeline))]
-    //[Dependency(typeof(PushNuGetPackagesToQueo))]
     public partial class Default { }
 
-    [IsDependentOn(typeof(FormatCheck))]
+    //[IsDependentOn(typeof(FormatCheck))]
     [IsDependentOn(typeof(NugetRestore))]
     [IsDependentOn(typeof(GenerateVersion))]
     [IsDependentOn(typeof(Build))]
